084398305a29d5f646465c486aa2295fb31f933c
Kerry Mahne <kerry.mahne@tretton37.com>
2019-11-13 16:28:43 +0000

    added option to post tweet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const { hostname } = require('os')
 const AutoLoad = require('fastify-autoload')
 
 module.exports = function (fastify, opts, next) {
@@ -11,18 +12,35 @@ module.exports = function (fastify, opts, next) {
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
   })
-
   // This loads all plugins defined in services
   // define your routes in one of these
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'services'),
     options: Object.assign({}, opts)
   })
 
+  fastify.register(
+    require('fastify-elasticsearch'), {
+      cloud: {
+        id: 'fastify-workshop:ZXVyb3BlLXdlc3QyLmdjcC5lbGFzdGljLWNsb3VkLmNvbSQzOGU4ZWI4ODY1ZmU0ZDI5OGY1Y2FiMmQzZjQ4OTc0MiRkMjYwZGYxNWExZjY0YWM1OTU2NjY1ZTJkOTk3OGNhOA=='
+      },
+      auth: {
+        username: 'workshop',
+        password: 'workshop-42'
+      }
+    }
+  )
+
+  fastify.register(
+    require('@delvedor/fastify-workshop-dataset'),
+    { indexName: `${hostname().replace(/[^-_A-Za-z0-9]/g, '_').toLowerCase()}-tweets` }
+  )
   // Make sure to call next when done
   next()
 }
---
+++ services/post/index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { hostname } = require('os')
+const hyperid = require('hyperid')
+const instance = hyperid()
+
+module.exports = async function (fastify, opts) {
+  fastify.route({
+    method: 'POST',
+    url: '/tweet',
+    onRequest: fastify.basicAuth,
+    handler: async (request, reply) => {
+      const id = instance()
+      const {name, topics} = request.user;
+      const body = {
+        id,
+        user: name,
+        topics,
+        time: new Date(),
+        text: request.body.text || 'watevr'
+      }
+
+      await fastify.elastic.index({
+        index: `${hostname().replace(/[^-_A-Za-z0-9]/g, '_').toLowerCase()}-tweets`,
+        refresh: 'wait_for',
+        id,
+        body
+
+      })
+      return { id }
+    }
+  })
+}
